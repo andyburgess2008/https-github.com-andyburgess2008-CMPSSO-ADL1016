6475aa7e81a30dbb554ec6c6a5127f6a8b822443
Anda.Luo <Anda.Luo>
2016-10-12 11:10:40 -0700

    update config files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /*eslint-env node*/
+/*eslint-env node*/
 
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
@@ -12,17 +13,105 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 // create a new express server
 var app = express();
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'public'));
+// anda add some here
+
+//
+var port = (process.env.VCAP_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+// serve the files out of ./public as our main files
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+app.use(cookieParser())
+ //new line *****
+//app.use(cookieParser());
+app.use(session({resave: 'true', saveUninitialized: 'true' , secret: 'keyboard cat'}));
+app.use(passport.initialize());
+app.use(passport.session()); 
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+passport.serializeUser(function(user, done) {
+   done(null, user);
+}); 
+
+passport.deserializeUser(function(obj, done) {
+   done(null, obj);
+});         
+
+// VCAP_SERVICES contains all the credentials of services bound to
+// this application. For details of its content, please refer to
+// the document or sample of each service.  
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+console.log("***",services.SingleSignOn);
+var ssoConfig = services.SingleSignOn[0]; 
+var client_id = ssoConfig.credentials.clientId;
+var client_secret = ssoConfig.credentials.secret;
+var authorization_url = ssoConfig.credentials.authorizationEndpointUrl;
+var token_url = ssoConfig.credentials.tokenEndpointUrl;
+var issuer_id = ssoConfig.credentials.issuerIdentifier;
+var callback_url = 'https://CMPSSO-ADL1016.mybluemix.net/auth/sso/callback';        
+
+var OpenIDConnectStrategy = require('passport-idaas-openidconnect').IDaaSOIDCStrategy;
+var Strategy = new OpenIDConnectStrategy({
+                 authorizationURL : authorization_url,
+                 tokenURL : token_url,
+                 clientID : client_id,
+                 scope: 'openid',
+                 response_type: 'code',
+                 clientSecret : client_secret,
+                 callbackURL : callback_url,
+                 skipUserProfile: true,
+                 issuer: issuer_id}, 
+	function(iss, sub, profile, accessToken, refreshToken, params, done)  {
+	         	process.nextTick(function() {
+		profile.accessToken = accessToken;
+		profile.refreshToken = refreshToken;
+		done(null, profile);
+         	})
+}); 
+
+passport.use(Strategy); 
+app.get('/login', passport.authenticate('openidconnect', {})); 
+          
+function ensureAuthenticated(req, res, next) {
+	if(!req.isAuthenticated()) {
+	          	req.session.originalUrl = req.originalUrl;
+		res.redirect('/login');
+	} else {
+		return next();
+	}
+}
+ 
+ //second add line
+ app.get('/auth/sso/callback',function(req,res,next) {               
+             var redirect_url = req.session.originalUrl;                
+             passport.authenticate('openidconnect', {
+                     successRedirect: redirect_url,                                
+                     failureRedirect: '/failure',                        
+          })(req,res,next);
+        });
+
+ //
+ app.get('/failure', function(req, res) { 
+             res.send('login failed'); });
+
+
+
+//my old line ****
+app.get('/home', function(req, res) {
+  res.send("Hello SSO"+req.cookies.value);
+  console.log('Cookies: ', req.cookies)
+})
+// start server on the specified port and binding hostnew
+app.listen(appEnv.port, host, function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
